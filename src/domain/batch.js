cf3b61620b9bbf776cdb39446397f75fa71072a8
ohbin-kwon <kobbb2100@gmail.com>
2022-11-05 18:33:09 +0900

    test: add concept of value object
    - domain/batch.js : add description of value object
    - test/batch.test.js : line object is immuatable object

--- src/domain/batch.js
+++ src/domain/batch.js
@@ -1,5 +1,13 @@
+// OrderLine 클래스로 생성된 객체는 식별자가 없다.
+// 데이터는 있지만, 식별자가 없는 비즈니스 개념을 값객체라고 표현한다.
+// 즉, 객체안의 데이터에 따라 식별될수있는 도메인 객체를 의미한다.
+// 예를 들어, 우리가 1000원을 말할때, 1000원의 가치가 중요하지, 1000원의 지폐가 어떤것인지는 중요하지 않은 것과 같은 의미이다.
+// 따라서, 객체안의 데이터가 바뀌면 더 이상 의미가 없어지는게 값객체이다.
+// 그래서 값객체는 불변객체로서 사용해야한다.
+// js에서 불변객체를 만들기위해서는 1. 컨벤션을 가지고 필요한 부분에 불변객체를 사용하거나 2. 인간을 믿지 못한다면 라이브러리를 활용해 강제해야한다.
+// 1번을 채택하고, OrderLIne으로 객체를 생성할때는 불변객체로 생성한다.
 export class OrderLine {
-  constructor(orderId, sku, quantity) {
+  constructor(orderId, sku, quantity) { // orderId는 Order의 식별자이지, OrderLine의 식별자가 아니다.
     this.orderId = orderId
     this.sku = sku
     this.quantity = quantity
--- src/test/batch.test.js
+++ src/test/batch.test.js
@@ -3,6 +3,7 @@ import {Batch, OrderLine} from "../domain/batch"
 test('test allocating to a batch reduces the available quantity', () => {
   const batch = new Batch("batch-001", "SMALL-TABLE", 20, Date.now())
   const line = new OrderLine('order-ref', "SMALL-TABLE", 2)
+  Object.freeze(line)
 
   batch.allocate(line)
 
