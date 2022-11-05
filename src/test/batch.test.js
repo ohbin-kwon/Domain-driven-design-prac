5f7714078029f2c20fa83f81ad79d1baf5316090
ohbin-kwon <kobbb2100@gmail.com>
2022-11-05 20:39:00 +0900

    test : add deallocate test
    - add deallocate test

--- src/test/batch.test.js
+++ src/test/batch.test.js
@@ -1,15 +1,5 @@
 import { Batch, OrderLine } from '../domain/batch';
-
-test('test allocating to a batch reduces the available quantity', () => {
-  const batch = new Batch('batch-001', 'SMALL-TABLE', 20, Date.now());
-  const line = new OrderLine('order-123', 'SMALL-TABLE', 2);
-  Object.freeze(line);
-
-  batch.allocate(line);
-
-  expect(batch.availableQuantity).toBe(18);
-});
-describe('can allocate test', () => {
+describe('allocate test', () => {
   // batchQuantity, lineQuantity에 맞는 batch와 line을 만들어내는 함수
   function makeBatchAndLine(sku, batchQuantity, lineQuantity) {
     return [
@@ -41,4 +31,44 @@ describe('can allocate test', () => {
     Object.freeze(differentSkuLine);
     expect(batch.canAllocate(differentSkuLine)).toBe(false);
   });
+
+  test('test allocating to a batch reduces the available quantity', () => {
+    const [batch, line] = makeBatchAndLine('LAMP', 20, 2);
+    batch.allocate(line);
+  
+    expect(batch.availableQuantity).toBe(18);
+  });
+});
+
+describe('deallocate test', () => {
+  function makeBatchAndLine(sku, batchQuantity, lineQuantity) {
+    return [
+      new Batch('batch-001', sku, batchQuantity, Date.now()),
+      Object.freeze(new OrderLine('order-123', sku, lineQuantity)),
+    ];
+  }
+
+  test('test can allocate allocated lines', () => {
+    const [batch, line] = makeBatchAndLine('LAMP', 20, 2);
+    batch.allocate(line);
+    batch.deallocate(line);
+    expect(batch.availableQuantity).toBe(20);
+  });
+
+  test('test can only deallocate allocated lines', () => {
+    const [batch, line] = makeBatchAndLine('LAMP', 20, 2);
+    const unallocatedLine = new OrderLine('order-123', 'SMALL-TABLE', 3);
+    Object.freeze(unallocatedLine);
+
+    batch.allocate(line);
+    batch.deallocate(unallocatedLine);
+    expect(batch.availableQuantity).toBe(18);
+  });
+
+  test('test allocation is idempotent', () => {
+    const [batch, line] = makeBatchAndLine('LAMP', 20, 2);
+    batch.allocate(line)
+    batch.allocate(line)
+    expect(batch.availableQuantity).toBe(18);
+  })
 });
