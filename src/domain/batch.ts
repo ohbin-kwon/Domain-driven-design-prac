708f0438b87cebd1dc9c67267e8e37c69d953677
ohbin-kwon <kobbb2100@gmail.com>
2022-11-16 17:37:25 +0900

    chore : delete set interface
    - set have a lot of interface and complexive type, so delete

--- src/domain/batch.ts
+++ src/domain/batch.ts
@@ -3,19 +3,6 @@ enum AllocateResult {
   'OUT-OF-STOCK' = 'out of stock',
   'DIFFERENT-SKU' = 'sku is different',
 }
-
-interface Set<T> {
-  add(value: T): this;
-  clear(): void;
-  delete(value: T): boolean;
-  forEach(
-    callbackfn: (value: T, value2: T, set: Set<T>) => void,
-    thisArg?: any,
-  ): void;
-  has(value: T): boolean;
-  readonly size: number;
-}
-
 interface ExistedEtaBatch extends Batch {
   eta: Date;
 }
