1cfb69283b93d0baf031ce92655fcf3677907d73
ohbin-kwon <kobbb2100@gmail.com>
2022-11-19 16:22:21 +0900

    feat : add service
    - allocate orderline to batches
    - need to check valid sku

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,11 +1,20 @@
-import { OrderLine } from "../domain/batch";
+import { allocate, Batch, OrderLine } from "../domain/batch";
 import { IRepository } from "../repository/IRepository";
 import { IService } from "./IService";
 
 export function service(): IService {
+  function isValidSku(sku: string, batches: Batch[]) {
+    return batches.find(batch => batch.sku === sku)
+  }
   return {
     async allocate(orderLine: OrderLine, repo: IRepository){
-      return ''
+      const batchList = await repo.list()
+      const validSku = isValidSku(orderLine.sku, batchList)
+      
+      if(validSku === undefined) return 'invalid sku'
+
+      const batchId = allocate(orderLine, batchList)
+      return batchId
     }
   }
 }
