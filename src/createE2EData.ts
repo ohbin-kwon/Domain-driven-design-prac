7e0f3016d6686bf029fab9aaa2bf7d051fadcecd
ohbin-kwon <kobbb2100@gmail.com>
2022-11-20 18:21:41 +0900

    test : add e2e dummy data generator
    - e2e service need dummy data
    - createE2E data is interface of create e2e dummy data
    - e2e need to inject specific repository

---
+++ src/createE2EData.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import { Batch } from './domain/batch';
+import { IRepository } from './repository/IRepository';
+
+export async function createE2EData(
+  setupRepo: () => Promise<IRepository>,
+  batches: Batch[],
+) {
+  const repo = await setupRepo()
+  Promise.all(batches.map(batch => repo.save(batch)))
+}
