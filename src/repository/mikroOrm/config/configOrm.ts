a342d051ab8e8d18fc2009148c81c0a76044ca27
ohbin-kwon <kobbb2100@gmail.com>
2022-11-24 21:43:33 +0900

    refact : use config module
    - use config module instead env variable
    - rename module name mikroOrm/config/config to mikroOrm/config/configOrm

---
+++ src/repository/mikroOrm/config/configOrm.ts
@@ -0,0 +1,12 @@
+import config from '../../../config';
+import { BatchEntity } from '../BatchEntity';
+import { OrderLineEntity } from '../OrderLineEntity';
+
+export default {
+  entities: [BatchEntity, OrderLineEntity],
+  dbName: config.POSTGRES_DB_NAME,
+  user: config.POSTGRES_USER_NAME,
+  password: config.POSTGRES_PASSWORD,
+  port: config.POSTGRES_PORT,
+  type: "postgresql" as "postgresql"
+};
