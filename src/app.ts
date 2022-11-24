a00a093a916a954cf906500a8df988d5803bc778
ohbin-kwon <kobbb2100@gmail.com>
2022-11-24 13:42:21 +0900

    feat : add allocate api
    - add allocate api
    - split app and server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import express from "express";
+import { OrderLine } from "./domain/batch";
+import { configMikroOrm } from "./repository/mikroOrm/config/configE2E";
+import { service } from "./service/service";
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+
+
+app.post('/allocate', async (req, res) => {
+  const {orderId, sku, qty} = req.body
+
+  const repo = await configMikroOrm()
+  const line = new OrderLine(orderId, sku, qty)
+  try{
+    const batchId = await service().allocate(line, repo)
+    res.status(201).send({batchId})
+  }catch(error){
+    let message;
+    if(error instanceof Error) message = error.message
+    else message = String(error)
+    res.status(400).send({message})
+  }
 })
 
-app.listen(8080, () => console.log("http://localhost:8080"));
+export default app
