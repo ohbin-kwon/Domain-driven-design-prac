af2722b591def9c98073288e8b3b845ae2b0f4b4
ohbin-kwon <kobbb2100@gmail.com>
2022-11-29 20:41:37 +0900

    test : add unit of work test factory

---
+++ src/service/uow/createUowTest.ts
@@ -0,0 +1,32 @@
+import { Batch } from '../../domain/batch';
+import { IUnitOfWork } from './IUow';
+
+// this test factory is for Uow implementation, fake Uow
+export function createUowTest(
+  label: string,
+  setUpUow: () => Promise<IUnitOfWork>,
+  teardown: (env: NODE_ENV) => Promise<void> = async () => undefined,
+) {
+  describe(label, () => {
+    afterEach(async () => {
+      await teardown('test');
+    });
+    it('test commit', async () => {
+      const uow = await setUpUow()
+      const batch = new Batch('b1', 'CHAIR', 100)
+      await uow.batches.save(batch)
+      uow.commit()
+  
+      const list = await uow.batches.list()
+      expect(list).toStrictEqual([batch])
+    });
+    it('test rollback uncommitted work', async () => {
+      const uow = await setUpUow()
+      const batch = new Batch('b1', 'CHAIR', 100)
+      await uow.batches.save(batch)
+  
+      const list = await uow.batches.list()
+      expect(list).toStrictEqual([])
+    });
+  });
+}
--- src/service/uow/mikroOrm/uow.mikroOrm.test.ts
+++ src/service/uow/mikroOrm/uow.mikroOrm.test.ts
@@ -1,26 +1,9 @@
-import { Batch } from "../../../domain/batch";
 import { tearDownMikroOrm } from "../../../repository/mikroOrm/config/setupOrm";
+import { createUowTest } from "../createUowTest";
 import { MikroOrmUow } from "./uow.mikroOrm";
 
-describe('allocation service test', () => {
-  afterEach(async () => {
-    await tearDownMikroOrm('test');
-  });
-  it('test commit', async () => {
-    const batch = new Batch('b1', 'CHAIR', 100)
-    const uow = await MikroOrmUow()
-    await uow.batches.save(batch)
-    uow.commit()
-
-    const list = await uow.batches.list()
-    expect(list).toStrictEqual([batch])
-  });
-  it('test rollback uncommitted work', async () => {
-    const batch = new Batch('b1', 'CHAIR', 100)
-    const uow = await MikroOrmUow()
-    await uow.batches.save(batch)
-
-    const list = await uow.batches.list()
-    expect(list).toStrictEqual([])
-  });
-});
+createUowTest(
+  'mikroOrm unit of work test',
+  MikroOrmUow,
+  tearDownMikroOrm
+)
