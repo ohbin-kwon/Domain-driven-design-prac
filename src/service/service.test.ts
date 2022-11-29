bacecbaa2f7e0455aa896c622a746687258acbd0
ohbin-kwon <kobbb2100@gmail.com>
2022-11-29 19:13:40 +0900

    test : use unit of work in service test
    - fake unit of work commit is fake of code(fake repo) not a fake of third-party module(mikroOrm)
    - according to 'Don't mock what you don't own'

--- src/service/service.test.ts
+++ src/service/service.test.ts
@@ -1,26 +1,28 @@
 import { FakeRepository } from '../repository/fake/repository';
 import { IRepository } from '../repository/IRepository';
 import { service } from './service';
+import { IUnitOfWork } from './uow/IUow';
+import { fakeUow } from './uow/uow.fake';
 
 // 오케스트레이션을 분리해서 서비스 layer로 분리한다.
 describe('allocation service test', () => {
   it('test returns allocations', async () => {
-    const repo: IRepository = FakeRepository([]);
+    const uow: IUnitOfWork = fakeUow();
     const batchId = 'b1'
     const sku = 'TABLE';
     const batchQuantity = 100
     const orderId = '1';
     const OrderQuantity = 10;
 
-    await service().addBatch(repo, batchId, sku, batchQuantity)
-
-    const result = await service().allocate(repo, orderId, sku, OrderQuantity);
-
+    await service().addBatch(uow, batchId, sku, batchQuantity)
+    
+    const result = await service().allocate(uow, orderId, sku, OrderQuantity);
+    
     expect(result).toStrictEqual('b1');
   });
 
   it('test error for invalid sku', async () => {
-    const repo: IRepository = FakeRepository([]);
+    const uow: IUnitOfWork = fakeUow();
     const batchId = 'b1'
     const batchSku = 'LAMP'
     const batchQuantity = 100
@@ -28,10 +30,11 @@ describe('allocation service test', () => {
     const orderSku = 'CHAIR';
     const orderQuantity = 10;
 
-    await service().addBatch(repo, batchId, batchSku, batchQuantity)
+    await service().addBatch(uow, batchId, batchSku, batchQuantity)
+    uow.commit()
 
     expect(
-      async () => await service().allocate(repo, orderId, orderSku, orderQuantity),
+      async () => await service().allocate(uow, orderId, orderSku, orderQuantity),
     ).rejects.toThrow(new Error('invalid sku - ' + orderSku));
   });
 });
