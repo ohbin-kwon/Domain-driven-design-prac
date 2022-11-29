cee692b138f3d892f9ee3aaedb86839c5adfac7b
ohbin-kwon <kobbb2100@gmail.com>
2022-11-29 18:40:41 +0900

    test :mikroOrm unit of work test

---
+++ src/service/uow/uow.mikroOrm.test.ts
@@ -0,0 +1,26 @@
+import { Batch } from "../../domain/batch";
+import { tearDownMikroOrm } from "../../repository/mikroOrm/config/setupOrm";
+import { MikroOrmUow } from "./uow.mikroOrm";
+
+describe('allocation service test', () => {
+  afterEach(async () => {
+    await tearDownMikroOrm('test');
+  });
+  it('test commit', async () => {
+    const batch = new Batch('b1', 'CHAIR', 100)
+    const uow = await MikroOrmUow()
+    await uow.batches.save(batch)
+    uow.commit()
+
+    const list = await uow.batches.list()
+    expect(list).toStrictEqual([batch])
+  });
+  it('test rollback uncommitted work', async () => {
+    const batch = new Batch('b1', 'CHAIR', 100)
+    const uow = await MikroOrmUow()
+    await uow.batches.save(batch)
+
+    const list = await uow.batches.list()
+    expect(list).toStrictEqual([])
+  });
+});
