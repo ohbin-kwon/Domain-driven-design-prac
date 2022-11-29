17e3ec0176b0e05a24a8269ac4b96a986fa73e19
ohbin-kwon <kobbb2100@gmail.com>
2022-11-29 18:49:26 +0900

    test : fake unit of work for service test

---
+++ src/service/uow/uow.fake.ts
@@ -0,0 +1,16 @@
+import { IUnitOfWork } from './IUow';
+import { FakeRepository } from '../../repository/fake/repository';
+
+export function fakeUow(): IUnitOfWork {
+  const repo = FakeRepository([])
+
+  return {
+    batches: repo,
+    commit() {
+      return;
+    },
+    rollback() {
+      return;
+    },
+  };
+}
