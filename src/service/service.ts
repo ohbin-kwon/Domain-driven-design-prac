8b9f85c8d54a7939de95f52a43cdad6161030fa0
ohbin-kwon <kobbb2100@gmail.com>
2022-12-01 15:44:32 +0900

    feat : use transaction in service

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,6 +1,6 @@
 import { allocate, Batch, OrderLine } from '../domain/batch';
-import { IRepository } from '../repository/IRepository';
 import { IService } from './IService';
+import { withTransaction } from './transaction';
 import { IUnitOfWork } from './uow/IUow';
 
 export function service(): IService {
@@ -14,15 +14,19 @@ export function service(): IService {
       sku: string,
       quantity: number,
     ) {
-      const batchList = await uow.batches.list();
+      const batchId = await withTransaction<string>(uow, async (uow) => {
+        const batchList = await uow.batches.list();
 
-      const validSku = isValidSku(sku, batchList);
-      if (validSku === undefined) throw new Error('invalid sku - ' + sku);
+        const validSku = isValidSku(sku, batchList);
+        if (validSku === undefined) throw new Error('invalid sku - ' + sku);
 
-      const line = new OrderLine(orderId, sku, quantity);
+        const line = new OrderLine(orderId, sku, quantity);
 
-      const batchId = allocate(line, batchList);
-      uow.commit()
+        const batchId = allocate(line, batchList);
+        await uow.commit();
+        return batchId;
+      });
+
       return batchId;
     },
     async addBatch(
@@ -32,8 +36,10 @@ export function service(): IService {
       quantity: number,
       eta?: Date,
     ) {
-      await uow.batches.save(new Batch(id, sku, quantity, eta));
-      uow.commit()
+      await withTransaction(uow, async (uow) => {
+        await uow.batches.save(new Batch(id, sku, quantity, eta));
+        await uow.commit();
+      });
     },
   };
 }
