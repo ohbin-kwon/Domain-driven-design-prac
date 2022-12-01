00a916cc30bee956c0ddd66451f1b84366f85210
ohbin-kwon <kobbb2100@gmail.com>
2022-12-01 15:43:18 +0900

    feat : add transaction module
    - add transaction module

---
+++ src/service/transaction.ts
@@ -0,0 +1,13 @@
+import { IUnitOfWork } from "./uow/IUow"
+
+export async function withTransaction<T>(uow: IUnitOfWork, job: (uow: IUnitOfWork) => Promise<T>){
+  try {
+    uow.enter()
+    return await job(uow)
+  }catch(e){
+    uow.rollback()
+    throw e
+  }finally{
+    uow.exit()
+  }
+}
