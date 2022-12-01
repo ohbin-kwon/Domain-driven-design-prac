5621477a1c958fe2f710b134cd44b2cbadc4a33b
ohbin-kwon <kobbb2100@gmail.com>
2022-12-01 16:28:04 +0900

    test : add fake unit of work test
    - test uow.committted

---
+++ src/service/uow/fake/uow.fake.test.ts
@@ -0,0 +1,30 @@
+import { Batch } from '../../../domain/batch';
+import { withTransaction } from '../../transaction';
+import { FakeUow } from './uow.fake';
+
+describe('fake uow test', () => {
+  let init: Batch[] = [];
+
+  afterEach(async () => {
+    init.splice(0, init.length);
+  });
+  it('test commit', async () => {
+    const uow = FakeUow(init);
+    const batch = new Batch('b1', 'CHAIR', 100);
+    await withTransaction(uow, async (uow) => {
+      await uow.batches.save(batch);
+      uow.commit();
+    });
+
+    expect(uow.committed).toStrictEqual(true);
+  });
+  it('test rollback uncommitted work', async () => {
+    const uow = FakeUow(init);
+    const batch = new Batch('b1', 'CHAIR', 100);
+    await withTransaction(uow, async (uow) => {
+      await uow.batches.save(batch);
+    });
+
+    expect(uow.committed).toStrictEqual(false);
+  });
+});
