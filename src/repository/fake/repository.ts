809b1c9f711cb6f946cd676c2118948ff53a6189
ohbin-kwon <kobbb2100@gmail.com>
2022-12-04 15:35:01 +0900

    feat : repository - modify fake repository test
    - to test service, change fake double first(contain uow)

--- src/repository/fake/repository.ts
+++ src/repository/fake/repository.ts
@@ -1,22 +1,24 @@
-import { Batch } from '../../domain/batch';
-import { BatchSpecificProps, Filter, IRepository } from '../IRepository';
+import { Product } from '../../domain/product';
+import { ProductSpecificProps, Filter, IRepository } from '../IRepository';
 
-export function FakeRepository(init: Batch[]): IRepository {
+export function FakeRepository(init: Product[]): IRepository {
   const repo = init;
   return {
-    async list(): Promise<Batch[]> {
+    async list(): Promise<Product[]> {
       return repo;
     },
-    async get<T extends BatchSpecificProps>(filter: Filter<T>): Promise<Batch | null> {
+    async get<T extends ProductSpecificProps>(
+      filter: Filter<T>,
+    ): Promise<Product | null> {
       let targetColumn = Object.keys(filter)[0] as T;
       const targetBatch = repo.find(
-        (batch) => batch[targetColumn] === filter[targetColumn],
+        (product) => product[targetColumn] === filter[targetColumn],
       );
       if (targetBatch === undefined) return null;
       return targetBatch;
     },
-    async save(batch: Batch) {
-      repo.push(batch);
+    async save(product: Product) {
+      repo.push(product);
     },
   };
 }
