7b4618e91d711f8bf34bf2d63759dd5a97d2b6b6
ohbin-kwon <kobbb2100@gmail.com>
2022-12-10 06:18:43 +0900

    feat : global Allocate Result

--- src/global.ts
+++ src/global.ts
@@ -5,6 +5,11 @@ declare global {
   type NODE_ENV = 'test' | 'development' | 'production';
   type SESSION = SqlEntityManager<PostgreSqlDriver>;
   type GetKeysByValueType<Obj, Type> = {
-    [Prop in keyof Obj]-?: Obj[Prop] extends Type ? Prop : never
-  }[keyof Obj]
+    [Prop in keyof Obj]-?: Obj[Prop] extends Type ? Prop : never;
+  }[keyof Obj];
+  const enum AllocateResult {
+    'SUCCESS' = 'allocation success',
+    'OUT-OF-STOCK' = 'batch is out of stock',
+    'DIFFERENT-SKU' = 'sku is different',
+  }
 }
