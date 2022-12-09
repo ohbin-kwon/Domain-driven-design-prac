f10a4632ca4a02ac4dfdab38f05729321c9e31dc
ohbin-kwon <kobbb2100@gmail.com>
2022-12-10 02:10:05 +0900

    test : modify mikroOrm unit of work test

--- src/service/uow/mikroOrm/uow.mikroOrm.test.ts
+++ src/service/uow/mikroOrm/uow.mikroOrm.test.ts
@@ -1,5 +1,15 @@
-import { tearDownMikroOrm } from '../../../repository/mikroOrm/config/setupOrm';
+import {
+  connectMikroOrm,
+  setUpMikroOrmSession,
+  tearDownMikroOrm,
+} from '../../../repository/mikroOrm/config/setupOrmTest';
 import { createUowTest } from '../createUowTest';
 import { MikroOrmUow } from './uow.mikroOrm';
 
-createUowTest('mikroOrm unit of work test', MikroOrmUow, tearDownMikroOrm);
+createUowTest(
+  'mikroOrm unit of work test',
+  connectMikroOrm,
+  setUpMikroOrmSession,
+  MikroOrmUow,
+  tearDownMikroOrm,
+);
