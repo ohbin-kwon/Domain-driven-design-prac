5cc0a2be93b998b72d907fca89850fec5512db9e
ohbin-kwon <kobbb2100@gmail.com>
2022-12-10 02:15:42 +0900

    feat : add connect and session in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import { service } from './service/service';
 import { MikroOrmUow } from './service/uow/mikroOrm/uow.mikroOrm';
+import { connectMikroOrm, setUpMikroOrmSession } from './repository/mikroOrm/config/setupOrm';
 
 const app = express();
 
 app.use(express.json());
 
+connectMikroOrm()
+
 app.post('/batch', async (req, res) => {
   const { id, sku, qty, eta } = req.body;
   try {
-    const uow = await MikroOrmUow();
+    const uow = await MikroOrmUow(setUpMikroOrmSession);
     await service().addBatch(uow, id, sku, qty, eta);
     res.status(201).end();
   } catch (error) {
@@ -24,7 +27,7 @@ app.post('/allocate', async (req, res) => {
   const { orderId, sku, qty } = req.body;
 
   try {
-    const uow = await MikroOrmUow();
+    const uow = await MikroOrmUow(setUpMikroOrmSession);
     const batchId = await service().allocate(uow, orderId, sku, qty);
     res.status(201).send({ batchId });
   } catch (error) {
